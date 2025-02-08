c8c522384bdbab86bce5f88ab9d56babb1ba084a
Milosz <milosz.zawolik@gmail.com>
2025-02-08 12:43:44 +0100

    Added Logger functionality and few utility functions!

---
+++ src/logger/handlers.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import logging
+import colorlog # type: ignore
+
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+log_filepath = os.path.join(log_dir, "running_logs.log")
+
+log_format = "%(log_color)s[%(levelname)s]%(reset)s - %(message)s"
+
+log_colors = {
+    'INFO': 'green',
+    'DEBUG': 'blue',
+    'WARNING': 'yellow',
+    'ERROR': 'red',
+    'CRITICAL': 'magenta'
+}
+
+console_handler = colorlog.StreamHandler(sys.stdout)
+console_formatter = colorlog.ColoredFormatter(log_format, log_colors=log_colors)
+console_handler.setFormatter(console_formatter)
+
+logger = logging.getLogger("TextSummarization-Logger")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(log_filepath)
+file_formatter = logging.Formatter('%(levelname)s %(message)s')
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+logger.info("Logger is set up successfully.")
---
+++ src/utils/functions.py
@@ -0,0 +1,71 @@
+import os
+import yaml
+
+from box import ConfigBox
+from box.exceptions import BoxValueError # type: ignore
+from pathlib import Path
+from logger.handlers import logger
+from ensure import ensure_annotations
+
+@ensure_annotations
+def read_yaml(path_to_yaml_file: Path) -> ConfigBox:
+    """
+    Reads a yaml file and returns a ConfigBox object.
+
+    Args:
+        path_to_yaml_file: Path to the YAML file.
+
+    Returns:
+        ConfigBox: The content of the YAML file in a ConfigBox format.
+    """
+    try:
+        with open(path_to_yaml_file, "r") as file:
+            content = yaml.safe_load(file)
+            logger.info(f"Successfully read YAML file: {path_to_yaml_file}")
+            return ConfigBox(content)
+
+    except BoxValueError as e:
+        logger.error(f"Box error while reading YAML file: {path_to_yaml_file}")
+        raise e
+
+    except Exception as e:
+        logger.error(f"Unexpected error while reading YAML file: {path_to_yaml_file} - {str(e)}")
+        raise e
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """
+    Create a list of directories.
+
+    Args:
+        path_to_directories: List of directories to create.
+        verbose: Whether to print logs or not.
+    """
+    for path in path_to_directories:
+        try:
+            if not path.exists():
+                path.mkdir(parents=True, exist_ok=True)
+                if verbose:
+                    logger.info(f"Created directory: {path}")
+            else:
+                if verbose:
+                    logger.info(f"Directory already exists: {path}")
+
+        except Exception as e:
+            logger.error(f"Error creating {path}: {e}")
+
+
+@ensure_annotations
+def get_file_size(path: Path) -> str:
+    """
+    Get the size of a file in KB.
+
+    Args:
+        path: Path to the file.
+    
+    Returns:
+        str: Size of the file in KB.
+    """
+    size_in_kb = round(os.path.getsize(path) / 1024)
+    
+    return f"~ {size_in_kb} KB"
