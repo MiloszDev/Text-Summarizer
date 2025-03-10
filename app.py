eac7bd89aa728b7f4ea1a7dab849ab08ad2116b4
Milosz <milosz.zawolik@gmail.com>
2025-03-10 07:19:24 +0100

    Enabled HTTPS using SSL certificates

--- app.py
+++ app.py
@@ -34,4 +34,10 @@ async def predict_route(text: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(
+        app, 
+        host="0.0.0.0", 
+        port=8080, 
+        ssl_keyfile="key.pem", 
+        ssl_certfile="cert.pem"
+    )
